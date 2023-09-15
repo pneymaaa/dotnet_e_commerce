9b551bd44137673692a701eed43d52b15b40182d
pneymaaa <pneymila@gmail.com>
2023-09-15 21:48:51 +0700

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dotnet_e_commerce.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
